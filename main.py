e1d81c92e412970f0667b068b8feeb7e149063e0
Robert Antonius <robert.antonius23@gmail.com>
2024-06-01 23:07:15 +0700

    Asura and Dex down

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ import subprocess
 import sys
 import time
 import urllib
-from playwright.sync_api import sync_playwright
+from playwright.async_api import async_playwright
 
 # 0 = spesial, 1 = request, 2 = playwright, 3 = ???
 sites = [
-    ["https://mangadex.org/search?q=", "https://mangadex.org/title/", 0]
+    ["https://mangadex.org/search?q=", "https://mangadex.org/title/", 0],
+    ["https://asuratoon.com/?s=", "https://asuratoon.com/manga/", 1],
 ]
 
 results = []
@@ -37,41 +38,56 @@ capabilities = {
     },
 }
 
+# Untuk scrape situs yang bisa dihandali oleh request biasa
+async def search(site, title):
+    # "https://www.wuxiaworld.com/q=122221"
+    link = site[0] + title.replace(" ", "+")
+
+    raw_response = requests.get(link)
+    raw_response.encoding = 'utf-8'
+    soup = BeautifulSoup(raw_response.text, 'html.parser')
+    a_hrefs = soup.find_all("a", attrs={"href": re.compile("^"+site[1])})
+
+    print(soup.prettify())
+    print()
+
+    for a_href in a_hrefs:
+      results.append(a_href['href'])
+
 # Untuk scrape situs yang tidak bisa dihandali oleh request biasa (SPA, JS-heavy, dst)
-async def playwright(sites):
-    with sync_playwright() as playwright:
-        playwright_version = (
-            str(subprocess.getoutput("playwright --version")).strip().split(" ")[1]
-        )
+async def playwright(site, title):
+    async with async_playwright() as playwright:
+        playwright_version = str(subprocess.getoutput("playwright --version")).strip().split(" ")[1]
         capabilities["LT:Options"]["playwrightClientVersion"] = playwright_version
-        browser = playwright.chromium.launch(headless=False) 
-        page = browser.new_page()
-        try:            
-            # section to navigate to software category  
-            page.goto("https://ecommerce-playground.lambdatest.io/")
-            page.wait_for_selector("")
-        except Exception as ex:
-            print(str(ex))
-        finally:
-            print(page.inner_html())
+        browser = await playwright.chromium.launch(headless=True)
+        page = await browser.new_page()
+        await page.goto(site[0] + title)
+        
+        locator = page.locator(".listupd")
+        outerHTML = await locator.evaluate("el => el.outerHTML")
+        soup = BeautifulSoup("".join(outerHTML), 'html.parser')
+        a_hrefs = soup.find_all("a", attrs={"href": re.compile("^"+site[1])})
+        
+        for a_href in a_hrefs:
+            results.append(a_href['href'])
+        await browser.close()
 
 # MangaDex ada API-nya jadi gak ada alasan untuk nggak pake
 async def mangadex(title):
 # def mangadex(title):
     # Dari docs API https://api.mangadex.org/docs/swagger.html#/Manga/get-search-manga
     # Docs API sikok lagi buruk nian, yang ini biso simulate url
-    print("Loading...")
-    await asyncio.sleep(5)
-    print("Starting...")
     r = requests.get(f"https://api.mangadex.org/manga?title={title}&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&order%5Brelevance%5D=desc&hasAvailableChapters=true")
-    await asyncio.sleep(5)
-    print([f"https://mangadex.org/title/{manga['id']}" for manga in r.json()["data"]])
+    for manga in r.json()["data"]:
+        results.append(f"https://mangadex.org/title/{manga['id']}")
 
 async def main():
     await asyncio.gather(
         mangadex("a manga"), 
-        mangadex("a manga")
+        playwright(sites[1], "a")
     )
+    for result in results:
+        print(result)
 
 if __name__ == "__main__":
     asyncio.run(main())
