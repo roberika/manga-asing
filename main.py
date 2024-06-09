5da4815d56b0e78ba5fc58f23195a5f3e1d2b981
Robert Antonius <robert.antonius23@gmail.com>
2024-06-09 23:42:52 +0700

    Scrape the title and image as well
    With a LOT of refactoring

--- main.py
+++ main.py
@@ -16,117 +16,175 @@ from urllib.parse import unquote, urlparse
 from pathlib import PurePosixPath
 from playwright.async_api import async_playwright
 
-# header, bagian search, bagian manga, batasan segment, title, filter out
+# header, bagian search, bagian manga, batasan segment, title filter, filter out, image inside, filter title
 # 0 = spesial, 1 = request, 2 = playwright
-sites = [
-    ["https://mangadex.org", "/search?q=", "/title/", None, False, None],
-    ["https://asuratoon.com", "/?s=", "https://asuratoon.com/manga/", None, False, {"class" : "series"}],
-    ["https://mangapill.com", "/search?q=", "/manga/", None, False, None],
-    ["https://mangapark.net", "/search?word=", "/title/", None, False, None],
-    ["https://flamecomics.me", "/?s=", "https://flamecomics.me/series/", 3, True, None],
-    ["https://mangareader.to", "/search?keyword=", r'/.*-[0-9]+$', 2, True, None],
-    ["https://mangafire.to", "/filter?keyword=", '/manga/', None, False, None],
-    ["https://ww1.mangafreak.me", "/Find/", '/Manga/', None, False, None],
 
-]
+# buka gambar manga pill perlu referer https://mangapill.com/
 
-results = []
-
-capabilities = {
-    "browserName": "Chrome",  # Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
-    "browserVersion": "latest",
-    "LT:Options": {
-        "platform": "Windows 11",
-        "build": "Manga Asing Scrape Build",
-        "name": "Scrape Manga Search Results",
-        "network": False,
-        "video": True,
-        "console": True,
-        "tunnel": False,  # Localhost
-        "tunnelName": "",  # Optional, localhost dst
-        "geoLocation": "ID",
-    },
-}
-
-# Untuk scrape situs yang bisa dihandali oleh request biasa
-async def request(site, title):
-    link = site[0] + site[1] + title_url(title)
-
-    raw_response = requests.get(link)
-    raw_response.encoding = 'utf-8'
-    soup = BeautifulSoup(raw_response.text, 'html.parser')
-    a_hrefs = soup.find_all("a", attrs=({"href": re.compile("^"+site[2])}))
-
-    for a in a_hrefs:
-        print(a.prettify())
-    print(soup.prettify())
-    # Filter elemen yang punya title (flamescomic, mangareader)
-    if(site[4]):
-        a_hrefs = [a_href for a_href in a_hrefs if a_href.has_attr('title')]
-
-    # Filter elemen yang bukan manga
-    if(site[5] != None):
-        a_hrefs = [a_href for a_href in a_hrefs if a_href.find(attrs=site[5]) == None]
-    a_hrefs = [a_href["href"] for a_href in a_hrefs]
-
-    # Filter link ke halaman bab
-    if(site[3] != None): 
-        a_hrefs = ["/" + "/".join(PurePosixPath(unquote(urlparse(a_href).path)).parts[1:site[3]]) for a_href in a_hrefs]
-    a_hrefs = list(set(a_hrefs))[0:limit]
-
-    for a_href in a_hrefs:
-      results.append(site[0] + a_href)
-    
-    print(site[0])
-
-# Comick memakai JS untuk meload data, sehingga perlu diolah terpisah dengan playwright
-async def comick(title):
-    async with async_playwright() as playwright:
-        playwright_version = str(subprocess.getoutput("playwright --version")).strip().split(" ")[1]
-        capabilities["LT:Options"]["playwrightClientVersion"] = playwright_version
-        browser = await playwright.chromium.launch(headless=True)
-        page = await browser.new_page()
-        await page.goto("https://comick.io/search?q=" + title_url(title))
-        
-        loaded = page.locator("id=__next")
-        outerHTML = await loaded.evaluate("el => el.outerHTML")
-        soup = BeautifulSoup("".join(outerHTML), 'html.parser')
-        a_hrefs = soup.find_all("a", attrs=({"href": re.compile("^/comic/")}))
-        
-        for a_href in a_hrefs:
-            results.append(a_href)
-        await browser.close()
-
-# MangaDex ada API-nya jadi gak ada alasan untuk nggak pake
 async def mangadex(title):
 # def mangadex(title):
     # Dari docs API https://api.mangadex.org/docs/swagger.html#/Manga/get-search-manga
     # Docs API sikok lagi buruk nian, yang ini biso simulate url
-    r = requests.get(f"https://api.mangadex.org/manga?title={title_url(title)}&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&order%5Brelevance%5D=desc&hasAvailableChapters=true")
-    for manga in r.json()["data"][0:limit]:
-        results.append(f"https://mangadex.org/title/{manga['id']}")
+    site = "https://mangadex.org"
+    r = requests.get(f"https://api.mangadex.org/manga?title={title_url(title)}&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&order%5Brelevance%5D=desc&hasAvailableChapters=true&includes%5B%5D=cover_art")
+    
+    results = []
+    for manga in r.json()["data"]:
+        title = list(manga['attributes']['title'].values())[0]
+        id = manga['id']
+        image = [r['attributes']['fileName'] for r in manga['relationships'] if r['type'] == 'cover_art'][0]
+        image_url = f"https://uploads.mangadex.org/covers/{id}/{image}.512.jpg"
+        results.append(parse_manga(title, f"{site}/title/{id}", image_url))
+    return {"site": "MangaDex", "site_url": site, "results": results[0:limit]}
 
-    print("https://mangadex.org")
+async def asuratoons(title):
+    site = "https://asuratoon.com"
+    raw_response = requests.get(f"{site}/?s={title_url(title)}")
+    raw_response.encoding = 'utf-8'
+    soup = BeautifulSoup(raw_response.text, 'html.parser')
+    manga_list = soup.find_all("div", attrs={"class": re.compile("bsx")})
+
+    results = []
+    for manga in manga_list:
+        a_href = manga.find("a", attrs={"href": re.compile(f"^{site}/manga/"), "title": re.compile(r"\w")})
+        img = a_href.img
+
+        manga_title = a_href['title']
+        url = a_href['href']
+        image_url = img["src"]
+        results.append(parse_manga(manga_title, url, image_url))
+    return {"site": "Asura Toons", "site_url": site, "results": results[0:limit]}
+
+async def flamecomics(title):
+    site = "https://flamecomics.me"
+    raw_response = requests.get(f"{site}/?s={title_url(title)}")
+    raw_response.encoding = 'utf-8'
+    soup = BeautifulSoup(raw_response.text, 'html.parser')
+    manga_list = soup.find_all("div", attrs={"class": re.compile("bsx")})
+
+    results = []
+    for manga in manga_list:
+        a_href = manga.find("a", attrs={"href": re.compile(f"^{site}/series/"), "title": re.compile(r"\w")})
+        img = a_href.img
+
+        manga_title = a_href['title']
+        url = a_href['href']
+        image_url = img["src"]
+        results.append(parse_manga(manga_title, url, image_url))
+    return {"site": "Flame Comics", "site_url": site, "results": results[0:limit]}
+
+async def mangapill(title):
+    site = "https://mangapill.com"
+    raw_response = requests.get(f"{site}/search?q={title_url(title)}&type=&status=")
+    raw_response.encoding = 'utf-8'
+    soup = BeautifulSoup(raw_response.text, 'html.parser')
+    manga_list = [a.parent for a in soup.find_all("a", attrs={"href": re.compile("^/manga/")}) if len(a.parent.attrs) == 0]
+
+    results = []
+    for manga in manga_list:
+        a_href = manga.find("a", attrs={"class": re.compile("mb-2")})
+        img = manga.img
+        div = a_href.find()
+
+        manga_title = div.string
+        url = a_href['href']
+        image_url = img['data-src']
+        results.append(parse_manga(manga_title, url, image_url))
+    return {"site": "MangaPill", "site_url": site, "results": results[0:limit]}
+
+async def mangapark(title):
+    site = "https://mangapark.net"
+    raw_response = requests.get(f"{site}/search?word={title_url(title)}")
+    raw_response.encoding = 'utf-8'
+    soup = BeautifulSoup(raw_response.text, 'html.parser')
+    manga_list = soup.find_all("img", attrs={"title": re.compile(r"\w")})
+
+    results = []
+    for manga in manga_list:
+        a_href = manga.parent
+
+        manga_title = manga['title']
+        url = a_href['href']
+        image_url = manga['src']
+        results.append(parse_manga(manga_title, url, image_url))
+    return {"site": "MangaPark", "site_url": site, "results": results[0:limit]}
+
+async def mangareader(title):
+    site = "https://mangareader.to"
+    raw_response = requests.get(f"{site}/search?keyword={title_url(title)}")
+    raw_response.encoding = 'utf-8'
+    soup = BeautifulSoup(raw_response.text, 'html.parser')
+    manga_list = soup.find_all("div", attrs={"class": re.compile("item item-spc")})
+
+    results = []
+    for manga in manga_list:
+        a_href = manga.find('a', attrs={"title": re.compile(r"\w")})
+        img = manga.img
+
+        manga_title = a_href.string
+        url = a_href['href']
+        image_url = img['src']
+        results.append(parse_manga(manga_title, url, image_url))
+    return {"site": "MangaReader", "site_url": site, "results": results[0:limit]}
+
+async def mangafire(title):
+    site = "https://mangafire.to"
+    raw_response = requests.get(f"{site}/filter?keyword={title_url(title)}")
+    raw_response.encoding = 'utf-8'
+    soup = BeautifulSoup(raw_response.text, 'html.parser')
+    manga_list = soup.find_all("div", attrs={"class": re.compile("unit item")})
+
+    results = []
+    for manga in manga_list:
+        a_href = manga.find('div', attrs={"class": re.compile("info")}).find('a', attrs={"href": re.compile("/manga/")})
+        img = manga.img
+
+        manga_title = a_href.string
+        url = a_href['href']
+        image_url = img['src']
+        results.append(parse_manga(manga_title, url, image_url))
+    return {"site": "MangaFire", "site_url": site, "results": results[0:limit]}
+    
+async def mangafreak(title):
+    site = "https://ww1.mangafreak.me"
+    raw_response = requests.get(f"{site}/Find/{title_url(title)}")
+    raw_response.encoding = 'utf-8'
+    soup = BeautifulSoup(raw_response.text, 'html.parser')
+    manga_list = soup.find_all("div", attrs={"class": re.compile("manga_search_item")})
+
+    results = []
+    for manga in manga_list:
+        a_href = manga.h3.find('a', attrs={"href": re.compile("/Manga/")})
+        img = manga.img
+
+        manga_title = a_href.string
+        url = a_href['href']
+        image_url = img['src']
+        results.append(parse_manga(manga_title, url, image_url))
+    return {"site": "MangaFreak", "site_url": site, "results": results[0:limit]}
 
 def title_url(title):
     return urllib.parse.quote_plus(title)
 
-limit = 999
+def parse_manga(title, url, image):
+    return {"title": title, "url": url, "image": image}
+
+limit = 5
 
 async def search(title):
-    await asyncio.gather(
-        # mangadex(title), 
-        # request(sites[1], title),
-        # request(sites[2], title),
-        # request(sites[3], title),
-        # request(sites[4], title),
-        # request(sites[5], title),
-        # request(sites[6], title),
-        # request(sites[7], title),
+    results = await asyncio.gather(
+        mangadex(title), 
+        asuratoons(title),
+        mangapill(title),
+        mangapark(title),
+        flamecomics(title),
+        mangareader(title),
+        mangafreak(title),
     )
     for result in results:
-        print(result)
-
+        print(result['site'])
+        for index, manga in enumerate(result['results']):
+            print(f"{index+1}.\t{manga['title']}")
 
 if __name__ == "__main__":
     title = input("Search for: ")
