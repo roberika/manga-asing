c4118290ebfe9080bf43dafb1c366a421fa3b48c
Robert Antonius <robert.antonius23@gmail.com>
2024-06-11 14:45:54 +0700

    Finished with basic functions
    Just needs decorating

--- main.py
+++ main.py
@@ -14,171 +14,162 @@ import tkhtmlview as tkhtml
 
 # buka gambar manga pill perlu referer https://mangapill.com/
 
-async def mangadex(title):
+async def mangadex(title, session):
 # def mangadex(title):
     # Dari docs API https://api.mangadex.org/docs/swagger.html#/Manga/get-search-manga
     # Docs API sikok lagi buruk nian, yang ini biso simulate url
     site = "https://mangadex.org"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://api.mangadex.org/manga?title={title_url(title)}&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&order%5Brelevance%5D=desc&hasAvailableChapters=true&includes%5B%5D=cover_art") as response:
-            results = []
-            r = await response
-            for manga in r.json()["data"]:
-                title = list(manga['attributes']['title'].values())[0]
-                id = manga['id']
-                image = [r['attributes']['fileName'] for r in manga['relationships'] if r['type'] == 'cover_art'][0]
-                image_url = f"https://uploads.mangadex.org/covers/{id}/{image}.512.jpg"
-                results.append(parse_manga(title, f"{site}/title/{id}", image_url))
-    print(site)
+    print("Fetching", site)
+    async with session.get(f"https://api.mangadex.org/manga?title={title_url(title)}&contentRating%5B%5D=safe&contentRating%5B%5D=suggestive&contentRating%5B%5D=erotica&order%5Brelevance%5D=desc&hasAvailableChapters=true&includes%5B%5D=cover_art") as response:
+        results = []
+        r = await response.json()
+        for manga in r["data"]:
+            title = list(manga['attributes']['title'].values())[0]
+            id = manga['id']
+            image = [r['attributes']['fileName'] for r in manga['relationships'] if r['type'] == 'cover_art'][0]
+            image_url = f"https://uploads.mangadex.org/covers/{id}/{image}.512.jpg"
+            results.append(parse_manga(title, f"{site}/title/{id}", image_url))
+    print(site, "done")
     return {"site": "MangaDex", "site_url": site, "results": results[0:limit]}
 
-async def asuratoons(title):
+async def asuratoons(title, session):
     site = "https://asuratoon.com"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"{site}/?s={title_url(title)}") as response:
-            raw_response = await response
-            raw_response.encoding = 'utf-8'
-            soup = BeautifulSoup(raw_response.text, 'html.parser')
-            manga_list = soup.find_all("div", attrs={"class": re.compile("bsx")})
+    print("Fetching", site)
+    async with session.get(f"{site}/?s={title_url(title)}") as response:
+        raw_response = await response.text()
+    soup = BeautifulSoup(raw_response, 'html.parser')
+    manga_list = soup.find_all("div", attrs={"class": re.compile("bsx")})
 
-            results = []
-            for manga in manga_list:
-                a_href = manga.find("a", attrs={"href": re.compile(f"^{site}/manga/"), "title": re.compile(r"\w")})
-                img = a_href.img
+    results = []
+    for manga in manga_list:
+        a_href = manga.find("a", attrs={"href": re.compile(f"^{site}/manga/"), "title": re.compile(r"\w")})
+        img = a_href.img
 
-                manga_title = a_href['title']
-                url = a_href['href']
-                image_url = img["src"]
-                results.append(parse_manga(manga_title, url, image_url))
-    print(site)
+        manga_title = a_href['title']
+        url = a_href['href']
+        image_url = img["src"]
+        results.append(parse_manga(manga_title, url, image_url))
+    print(site, "done")
     return {"site": "AsuraToons", "site_url": site, "results": results[0:limit]}
 
-async def flamecomics(title):
+async def flamecomics(title, session):
     site = "https://flamecomics.me"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"{site}/?s={title_url(title)}") as response:
-            raw_response = await response
-            raw_response.encoding = 'utf-8'
-            soup = BeautifulSoup(raw_response.text, 'html.parser')
-            manga_list = soup.find_all("div", attrs={"class": re.compile("bsx")})
+    print("Fetching", site)
+    async with session.get(f"{site}/?s={title_url(title)}") as response:
+        raw_response = await response.text()
+    soup = BeautifulSoup(raw_response, 'html.parser')
+    manga_list = soup.find_all("div", attrs={"class": re.compile("bsx")})
 
-            results = []
-            for manga in manga_list:
-                a_href = manga.find("a", attrs={"href": re.compile(f"^{site}/series/"), "title": re.compile(r"\w")})
-                img = a_href.img
+    results = []
+    for manga in manga_list:
+        a_href = manga.find("a", attrs={"href": re.compile(f"^{site}/series/"), "title": re.compile(r"\w")})
+        img = a_href.img
 
-                manga_title = a_href['title']
-                url = a_href['href']
-                image_url = img["src"]
-                results.append(parse_manga(manga_title, url, image_url))
-    print(site)
+        manga_title = a_href['title']
+        url = a_href['href']
+        image_url = img["src"]
+        results.append(parse_manga(manga_title, url, image_url))
+    print(site, "done")
     return {"site": "FlameComics", "site_url": site, "results": results[0:limit]}
 
-async def mangapill(title):
+async def mangapill(title, session):
     site = "https://mangapill.com"
-    connector = aiohttp.TCPConnector(ssl=False)
-    async with aiohttp.ClientSession() as session:
-        session.connector = connector
-        async with session.get(f"{site}/search?q={title_url(title)}&type=&status=") as response:
-            raw_response = await response
-            raw_response.encoding = 'utf-8'
-            soup = BeautifulSoup(raw_response.text, 'html.parser')
-            manga_list = [a.parent for a in soup.find_all("a", attrs={"href": re.compile("^/manga/")}) if len(a.parent.attrs) == 0]
+    print("Fetching", site)
+    async with session.get(f"{site}/search?q={title_url(title)}&type=&status=") as response:
+        raw_response = await response.text()
+    soup = BeautifulSoup(raw_response, 'html.parser')
+    manga_list = [a.parent for a in soup.find_all("a", attrs={"href": re.compile("^/manga/")}) if len(a.parent.attrs) == 0]
 
-            results = []
-            for manga in manga_list:
-                a_href = manga.find("a", attrs={"class": re.compile("mb-2")})
-                img = manga.img
-                div = a_href.find()
+    results = []
+    for manga in manga_list:
+        a_href = manga.find("a", attrs={"class": re.compile("mb-2")})
+        img = manga.img
+        div = a_href.find()
 
-                manga_title = div.string
-                url = a_href['href']
-                image_url = img['data-src']
-                results.append(parse_manga(manga_title, url, image_url))
-    print(site)
+        manga_title = div.string
+        url = a_href['href']
+        image_url = img['data-src']
+        results.append(parse_manga(manga_title, url, image_url))
+    print(site, "done")
     return {"site": "MangaPill", "site_url": site, "results": results[0:limit]}
 
-async def mangapark(title):
+async def mangapark(title, session):
     site = "https://mangapark.net"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"{site}/search?word={title_url(title)}") as response:
-            raw_response = await response
-            raw_response.encoding = 'utf-8'
-            soup = BeautifulSoup(raw_response.text, 'html.parser')
-            manga_list = soup.find_all("img", attrs={"title": re.compile(r"\w")})
+    print("Fetching", site)
+    async with session.get(f"{site}/search?word={title_url(title)}") as response:
+        raw_response = await response.text()
+    soup = BeautifulSoup(raw_response, 'html.parser')
+    manga_list = soup.find_all("img", attrs={"title": re.compile(r"\w")})
 
-            results = []
-            for manga in manga_list:
-                a_href = manga.parent
+    results = []
+    for manga in manga_list:
+        a_href = manga.parent
 
-                manga_title = manga['title']
-                url = a_href['href']
-                image_url = manga['src']
-                results.append(parse_manga(manga_title, url, image_url))
-    print(site)
+        manga_title = manga['title']
+        url = a_href['href']
+        image_url = manga['src']
+        results.append(parse_manga(manga_title, url, image_url))
+    print(site, "done")
     return {"site": "MangaPark", "site_url": site, "results": results[0:limit]}
 
-async def mangareader(title):
+async def mangareader(title, session):
     site = "https://mangareader.to"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"{site}/search?keyword={title_url(title)}") as response:
-            raw_response = await response
-            raw_response.encoding = 'utf-8'
-            soup = BeautifulSoup(raw_response.text, 'html.parser')
-            manga_list = soup.find_all("div", attrs={"class": re.compile("item item-spc")})
+    print("Fetching", site)
+    async with session.get(f"{site}/search?keyword={title_url(title)}") as response:
+        raw_response = await response.text()
+    soup = BeautifulSoup(raw_response, 'html.parser')
+    manga_list = soup.find_all("div", attrs={"class": re.compile("item item-spc")})
 
-            results = []
-            for manga in manga_list:
-                a_href = manga.find('a', attrs={"title": re.compile(r"\w")})
-                img = manga.img
+    results = []
+    for manga in manga_list:
+        a_href = manga.find('a', attrs={"title": re.compile(r"\w")})
+        img = manga.img
 
-                manga_title = a_href.string
-                url = a_href['href']
-                image_url = img['src']
-                results.append(parse_manga(manga_title, url, image_url))
-    print(site)
+        manga_title = a_href.string
+        url = a_href['href']
+        image_url = img['src']
+        results.append(parse_manga(manga_title, url, image_url))
+    print(site, "done")
     return {"site": "MangaReader", "site_url": site, "results": results[0:limit]}
 
-async def mangafire(title):
+async def mangafire(title, session):
     site = "https://mangafire.to"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"{site}/filter?keyword={title_url(title)}") as response:
-            raw_response = await response
-            raw_response.encoding = 'utf-8'
-            soup = BeautifulSoup(raw_response.text, 'html.parser')
-            manga_list = soup.find_all("div", attrs={"class": re.compile("unit item")})
+    print("Fetching", site)
+    async with session.get(f"{site}/filter?keyword={title_url(title)}") as response:
+        raw_response = await response.text()
+    soup = BeautifulSoup(raw_response, 'html.parser')
+    manga_list = soup.find_all("div", attrs={"class": re.compile("unit item")})
 
-            results = []
-            for manga in manga_list:
-                a_href = manga.find('div', attrs={"class": re.compile("info")}).find('a', attrs={"href": re.compile("/manga/")})
-                img = manga.img
+    results = []
+    for manga in manga_list:
+        a_href = manga.find('div', attrs={"class": re.compile("info")}).find('a', attrs={"href": re.compile("/manga/")})
+        img = manga.img
 
-                manga_title = a_href.string
-                url = a_href['href']
-                image_url = img['src']
-                results.append(parse_manga(manga_title, url, image_url))
-    print(site)
+        manga_title = a_href.string
+        url = a_href['href']
+        image_url = img['src']
+        results.append(parse_manga(manga_title, url, image_url))
+    print(site, "done")
     return {"site": "MangaFire", "site_url": site, "results": results[0:limit]}
     
-async def mangafreak(title):
+async def mangafreak(title, session):
     site = "https://ww1.mangafreak.me"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"{site}/Find/{title_url(title)}") as response:
-            raw_response = await response
-            raw_response.encoding = 'utf-8'
-            soup = BeautifulSoup(raw_response.text, 'html.parser')
-            manga_list = soup.find_all("div", attrs={"class": re.compile("manga_search_item")})
+    print("Fetching", site)
+    async with session.get(f"{site}/Find/{title_url(title)}") as response:
+        raw_response = await response.text()
+    soup = BeautifulSoup(raw_response, 'html.parser')
+    manga_list = soup.find_all("div", attrs={"class": re.compile("manga_search_item")})
 
-            results = []
-            for manga in manga_list:
-                a_href = manga.h3.find('a', attrs={"href": re.compile("/Manga/")})
-                img = manga.img
+    results = []
+    for manga in manga_list:
+        a_href = manga.h3.find('a', attrs={"href": re.compile("/Manga/")})
+        img = manga.img
 
-                manga_title = a_href.string
-                url = a_href['href']
-                image_url = img['src']
-                results.append(parse_manga(manga_title, url, image_url))
-    print(site)
+        manga_title = a_href.string
+        url = a_href['href']
+        image_url = img['src']
+        results.append(parse_manga(manga_title, url, image_url))
+    print(site, "done")
     return {"site": "MangaFreak", "site_url": site, "results": results[0:limit]}
 
 def title_url(title):
@@ -216,16 +207,16 @@ def parse_manga(title, url, image):
 #             print(f"{index+1}.\t{manga['title']}")
 
 async def aggregate(title):
-    asyncio.gather(
-        search_site(mangadex, title), 
-        search_site(asuratoons, title),
-        search_site(mangapill, title),
-        search_site(mangapark, title),
-        search_site(flamecomics, title),
-        search_site(mangareader, title),
-        search_site(mangafire, title),
-        search_site(mangafreak, title),
-    )
+    global canvas
+    async with aiohttp.ClientSession() as session:
+        funcs = [
+            mangadex, asuratoons, mangapill, mangapark,
+            flamecomics, mangareader, mangafire, mangafreak
+        ]
+        tasks = [
+            asyncio.create_task(coro=search_site(f, title, session)) for f in funcs
+        ]
+        await asyncio.gather(*tasks, return_exceptions=False)
 
 def search():
     global content_frame
@@ -233,11 +224,10 @@ def search():
     title = title_variable.get()
     asyncio.run(aggregate(title))
     
-async def search_site(func, title):
+async def search_site(func, title, session):
     global content_frame, canvas
-    root.geometry('799x600')
 
-    results = await func(title)
+    results = await func(title, session)
     site_frame = tk.Frame(content_frame, padx=5)
     site_frame.pack(fill=tk.X, side=tk.TOP)
     site_frame.grid_rowconfigure(1, weight=1, uniform='site_frame_row')
@@ -262,8 +252,6 @@ async def search_site(func, title):
         manga.bind("<Button-1>", lambda e, url=result['url']: webbrowser.open_new_tab(url))
         manga_image.bind("<Button-1>", lambda e, url=result['image']: webbrowser.open_new_tab(url))
         manga_title.bind("<Button-1>", lambda e, url=result['url']: webbrowser.open_new_tab(url))
-    
-    root.geometry('800x600')
 
 def main():
     global content_frame, canvas
@@ -312,8 +300,8 @@ def main():
     scrollbar = tk.Scrollbar(main_frame, orient=tk.VERTICAL, command=canvas.yview)
     scrollbar.grid(column=1, row=1, sticky='NS')
     canvas.configure(yscrollcommand=scrollbar.set)
-    canvas.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
     content_frame = tk.Frame(canvas, padx=10)
+    content_frame.bind('<Configure>', lambda e: canvas.configure(scrollregion=canvas.bbox("all")))
     canvas.create_window((0,0), window=content_frame, anchor='nw')
 
     root.mainloop()
